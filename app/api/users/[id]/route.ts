ccfeac71aa02d2ac18e69355926c448846f1fef7
nasanov <asanoff.n@gmail.com>
2025-06-03 00:27:40 -0400

    added a PATCH endpoint

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -16,3 +16,26 @@ export async function POST(req: NextRequest, { params }: { params: { id: string 
 		return NextResponse.json({ error: 'User update failed' }, { status: 500 });
 	}
 }
+
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+	const userId = params.id;
+	const body = await req.json();
+
+	try {
+		const user = await prisma.user.update({
+			where: { id: userId },
+			data: {
+				aboutMe: body.aboutMe,
+				birthdate: body.birthdate,
+				street: body.address?.street,
+				city: body.address?.city,
+				state: body.address?.state,
+				zip: body.address?.zip,
+			},
+		});
+
+		return NextResponse.json({ success: true, user });
+	} catch {
+		return NextResponse.json({ error: 'User update failed' }, { status: 500 });
+	}
+}

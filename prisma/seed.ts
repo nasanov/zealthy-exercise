553fc742012c64e9acdc322cf8c5bc4d0f922970
nasanov <asanoff.n@gmail.com>
2025-06-03 04:40:23 -0400

    added seeds for configs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,9 +9,18 @@ const userData: Prisma.UserCreateInput[] = [
 	},
 ];
 
+const componentConfigData: Prisma.ComponentConfigCreateInput[] = [
+	{ component: 'aboutMe', pageNumber: 2 },
+	{ component: 'address', pageNumber: 3 },
+	{ component: 'birthdate', pageNumber: 3 },
+];
+
 export async function main() {
-	for (const u of userData) {
-		await prisma.user.create({ data: u });
+	for (const user of userData) {
+		await prisma.user.create({ data: user });
+	}
+	for (const component of componentConfigData) {
+		await prisma.componentConfig.create({ data: component });
 	}
 }
 

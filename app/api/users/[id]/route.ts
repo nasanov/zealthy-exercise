41764c1dea112ff5d45a467f0c6ab07d9d40e3ac
nasanov <asanoff.n@gmail.com>
2025-06-03 02:07:43 -0400

    added GET endpoint, fixed an issue with date format

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,13 +1,30 @@
 import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
-	const userId = params.id;
+export async function GET(req: NextRequest) {
+	const id = req.nextUrl.pathname.split('/').pop();
+
+	try {
+		const user = await prisma.user.findUnique({
+			where: { id },
+		});
+		if (!user) {
+			return NextResponse.json({ error: 'User not found' }, { status: 404 });
+		}
+		return NextResponse.json({ user });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({ error: 'Failed to fetch user' }, { status: 500 });
+	}
+}
+
+export async function POST(req: NextRequest) {
+	const id = req.nextUrl.pathname.split('/').pop();
 	const body = await req.json();
 
 	try {
 		const user = await prisma.user.update({
-			where: { id: userId },
+			where: { id },
 			data: body,
 		});
 
@@ -17,16 +34,17 @@ export async function POST(req: NextRequest, { params }: { params: { id: string 
 	}
 }
 
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
-	const userId = params.id;
+export async function PATCH(req: NextRequest) {
+	const id = req.nextUrl.pathname.split('/').pop();
 	const body = await req.json();
 
+	const isoDate = new Date(body.birthdate).toISOString(); // "1999-01-01T00:00:00.000Z"
 	try {
 		const user = await prisma.user.update({
-			where: { id: userId },
+			where: { id },
 			data: {
 				aboutMe: body.aboutMe,
-				birthdate: body.birthdate,
+				birthdate: isoDate,
 				street: body.address?.street,
 				city: body.address?.city,
 				state: body.address?.state,
